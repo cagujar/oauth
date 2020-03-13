164161d61a5f23b07ef60430909ca06e7b4acbe8
cagujar <cagujar_180000001508@uic.edu.ph>
2020-03-13 20:11:12 +0800

    third commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 
 // create home route
 app.get('/', (req, res)=>{
-    res.render('home');
+    res.render('home',{description: "This is a sample description"});
 });
 
 // listen to requests
